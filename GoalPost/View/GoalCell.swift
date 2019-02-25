503c2ca844cccea7596200b1f1114515bec3fe33
Artur Ratajczak <artur.ratajczak1@gmail.com>
2019-02-25 18:56:23 +0100

    prepare goalCell UI

---
+++ GoalPost/View/GoalCell.swift
@@ -0,0 +1,22 @@
+//
+//  GoalCell.swift
+//  GoalPost
+//
+//  Created by Artur Ratajczak on 25/02/2019.
+//  Copyright © 2019 Artur Ratajczak. All rights reserved.
+//
+
+import UIKit
+
+class GoalCell: UITableViewCell {
+
+    @IBOutlet weak var goalDescriptionLbl: UILabel!
+    @IBOutlet weak var goalTypeLbl: UILabel!
+    @IBOutlet weak var goalProgressLbl: UILabel!
+    
+    func configureCell(description: String, goalType: String, goalProgress: Int){
+        goalDescriptionLbl.text = description
+        goalTypeLbl.text = goalType
+        goalProgressLbl.text = String(describing: goalProgress)
+    }
+}
